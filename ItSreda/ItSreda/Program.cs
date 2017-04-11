d527ecc6610732d57912775324a0610df3ce4957
egorikas <egorgrishechko@gmail.com>
2017-04-12 00:24:12 +0300

    add three examples about deadlocks
    add three examples about deadlocks.

--- ItSreda/ItSreda/Program.cs
+++ ItSreda/ItSreda/Program.cs
@@ -35,4 +35,5 @@ namespace ItSreda
             Console.WriteLine("Second");
         }
     }
+
 }
---
+++ ItSreda/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Block().Wait();
+            return View();
+        }
+
+        public async Task Block()
+        {
+            await Task.Delay(300);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
