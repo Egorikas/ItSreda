70304d57c6e1d19604f17e07c943f6a396d736f6
egorikas <egorgrishechko@gmail.com>
2017-04-14 00:02:44 +0300

    Working on the presentation
    Add some examples about threading

---
+++ ItSreda/Example1.ThreadingGame/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example1.ThreadingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example3().Wait();
+            Console.ReadLine();
+        }
+
+        static async Task Example1()
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(900);
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+
+        static async Task Example2()
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            await Task.FromResult(900);
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+
+
+        static async Task Example3()
+        {
+            Console.WriteLine($"Thread start of the method - {Thread.CurrentThread.ManagedThreadId}");
+            var firstTask = Task.Run(async () =>
+            {
+                Console.WriteLine($"firstTaskThreadId before - {Thread.CurrentThread.ManagedThreadId}");
+                await Task.Delay(900);
+                await Task.Delay(900);
+                await Task.Delay(900);
+                Console.WriteLine($"firstTaskThreadId after - {Thread.CurrentThread.ManagedThreadId}");
+            });
+            var secondTask = Task.Run(async () =>
+            {
+                Console.WriteLine($"secondTaskThreadId before - {Thread.CurrentThread.ManagedThreadId}");
+                await Task.Delay(900);
+                Console.WriteLine($"secondTaskThreadId after - {Thread.CurrentThread.ManagedThreadId}");
+            });
+            var thirdTask = Task.Run(async () =>
+            {
+                Console.WriteLine($"thirdTaskThreadId before - {Thread.CurrentThread.ManagedThreadId}");
+                await Task.Delay(900);
+                Console.WriteLine($"thirdTaskThreadId after - {Thread.CurrentThread.ManagedThreadId}");
+            });
+            Console.WriteLine($"Thread before Task.WhenAll - {Thread.CurrentThread.ManagedThreadId}");
+            await Task.WhenAll(firstTask, secondTask, thirdTask);
+            Console.WriteLine($"Thread after Task.WhenAll - {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
