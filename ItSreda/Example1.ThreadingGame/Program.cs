8b411e78eb2c5adccbcd75482d847724da2ef9de
egorikas <egorgrishechko@gmail.com>
2017-04-15 15:17:02 +0300

    add 3d example
    add 3d example about threads

--- ItSreda/Example1.ThreadingGame/Program.cs
+++ ItSreda/Example1.ThreadingGame/Program.cs
@@ -8,8 +8,7 @@ namespace Example1.ThreadingGame
     {
         static void Main(string[] args)
         {
-            Example3().Wait();
-            Console.ReadLine();
+            Example1().Wait();
         }
 
         static async Task Example1()
@@ -17,42 +16,6 @@ namespace Example1.ThreadingGame
             Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
             await Task.Delay(900);
             Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-        }
-
-        static async Task Example2()
-        {
-            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-            await Task.FromResult(900);
-            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-        }
-
-
-        static async Task Example3()
-        {
-            Console.WriteLine($"Thread start of the method - {Thread.CurrentThread.ManagedThreadId}");
-            var firstTask = Task.Run(async () =>
-            {
-                Console.WriteLine($"firstTaskThreadId before - {Thread.CurrentThread.ManagedThreadId}");
-                await Task.Delay(900);
-                await Task.Delay(900);
-                await Task.Delay(900);
-                Console.WriteLine($"firstTaskThreadId after - {Thread.CurrentThread.ManagedThreadId}");
-            });
-            var secondTask = Task.Run(async () =>
-            {
-                Console.WriteLine($"secondTaskThreadId before - {Thread.CurrentThread.ManagedThreadId}");
-                await Task.Delay(900);
-                Console.WriteLine($"secondTaskThreadId after - {Thread.CurrentThread.ManagedThreadId}");
-            });
-            var thirdTask = Task.Run(async () =>
-            {
-                Console.WriteLine($"thirdTaskThreadId before - {Thread.CurrentThread.ManagedThreadId}");
-                await Task.Delay(900);
-                Console.WriteLine($"thirdTaskThreadId after - {Thread.CurrentThread.ManagedThreadId}");
-            });
-            Console.WriteLine($"Thread before Task.WhenAll - {Thread.CurrentThread.ManagedThreadId}");
-            await Task.WhenAll(firstTask, secondTask, thirdTask);
-            Console.WriteLine($"Thread after Task.WhenAll - {Thread.CurrentThread.ManagedThreadId}");
         }
     }
 }
---
+++ ItSreda/Exmple3. ThreadingGame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exmple3.ThreadingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example1().Wait();
+            Console.ReadLine();
+        }
+
+        static async Task Example1()
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            await Task.FromResult(900);
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+
+
+        static async Task Example2()
+        {
+            Console.WriteLine($"Thread start of the method - {Thread.CurrentThread.ManagedThreadId}");
+            var firstTask = Task.Run(async () =>
+            {
+                Console.WriteLine($"Additional Task before - {Thread.CurrentThread.ManagedThreadId}");
+                await Task.Delay(900);
+                Console.WriteLine($"Additional Task after  - {Thread.CurrentThread.ManagedThreadId}");
+            });
+            Console.WriteLine($"Thread before Task.WhenAll - {Thread.CurrentThread.ManagedThreadId}");
+            await Task.WhenAll(firstTask);
+            Console.WriteLine($"Thread after Task.WhenAll - {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
